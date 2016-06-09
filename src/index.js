24d7ba811f96380452b91c8968117a18129044cf
ananthshenoyh <ananth@mediaiqdigital.com>
2016-06-09 17:51:24 +0530

    Sample UI lib

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var starWarsName=require('./starwars-name.json');
+var uniqueRandomArray=require('unique-random-array');
+module.exports={
+  all:starWarsName,
+  random:uniqueRandomArray(starWarsName)
+};
